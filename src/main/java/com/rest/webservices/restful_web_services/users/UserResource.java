46ef7b7d6cd4ee4dd3f2e138aac52e46e0505fee
Abdul Hannan <abdul.hannan.rcr@gmail.com>
2025-04-03 22:58:04 +0530

    HATEOS Hypermedia as the engine of application state

--- src/main/java/com/rest/webservices/restful_web_services/users/UserResource.java
+++ src/main/java/com/rest/webservices/restful_web_services/users/UserResource.java
@@ -3,6 +3,9 @@ package com.rest.webservices.restful_web_services.users;
 import java.net.URI;
 import java.util.List;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,18 +31,21 @@ public class UserResource {
 	}
 
 	@GetMapping(path = "/users/{id}")
-	public User retrieveUser(@PathVariable int id) {
+	public EntityModel<User> retrieveUser(@PathVariable int id) {
 		User user = service.findOne(id);
-		if(user==null) {
-			throw new UserNotFoundException("id: "+id);
+		if (user == null) {
+			throw new UserNotFoundException("id: " + id);
 		}
-		return user;
+		EntityModel<User> entityModel = EntityModel.of(user);
+		WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+		entityModel.add(link.withRel("all-users"));
+		return entityModel;
 	}
-	
+
 	@DeleteMapping(path = "/users/{id}")
 	public void deleteUser(@PathVariable int id) {
 		service.deleteById(id);
-		
+
 	}
 
 	@PostMapping(path = "/users")
